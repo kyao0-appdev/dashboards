cc1f7c9d04804dc59228cd00129ff556c2d94545
kyao0 <kyao0@chicagobooth.edu>
2022-10-29 20:13:25 +0000

    save

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get("/forex", { :controller => "currencies", :action => "first_currency"})
   get("/forex/:from_currency", { :controller => "currencies", :action => "second_currency"})
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get("/forex/:from_currency/:to_currency", { :controller => "currencies", :action => "convert_currency"})
+
 end
